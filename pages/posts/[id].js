2e0e565a7856083c4c58db4e6ca85fa762c5c6b4
brittanymbui <brittymbui@gmail.com>
2021-10-05 03:17:36 -0500

    Add data to Post component

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,5 +1,5 @@
 import Layout from '../../components/layout';
-import { getAllPostIds } from '../../lib/posts';
+import { getAllPostIds, getPostData } from '../../lib/posts';
 
 export async function getStaticPaths() {
     const paths = getAllPostIds()
@@ -9,8 +9,23 @@ export async function getStaticPaths() {
     }
 }
 
-export default function Post() {
-    return <Layout>
+export async function getStaticProps({ params }) {
+    const postData = getPostData(params.id)
+    return {
+        props: {
+            postData
+        }
+    }
+}
 
-    </Layout>
+export default function Post({ postData }) {
+    return (
+            <Layout>
+                {postData.title}
+                <br />
+                {postData.id}
+                <br />
+                {postData.date}
+            </Layout>
+    )
 }
