2590dd191788ca5ab205b359c106f106a5dc64de
brittanymbui <brittymbui@gmail.com>
2021-10-05 03:36:49 -0500

    Add styling to blog posts

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -3,6 +3,8 @@ import { getAllPostIds, getPostData } from '../../lib/posts';
 import Head from 'next/head'
 import Date from '../../components/date'
 
+import utilStyles from '../../styles/utils.module.css'
+
 export async function getStaticPaths() {
     const paths = getAllPostIds()
     return {
@@ -26,12 +28,13 @@ export default function Post({ postData }) {
                 <Head>
                 <title>{postData.title}</title>
                 </Head>
-                <br />
-                {postData.id}
-                <br />
-                <Date dateString={postData.date} />
-                <br />
-                <div dangerouslySetInnerHTML={{ __html: postData.contentHTML }} />
+                <article>
+                    <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+                    <div className={utilStyles.lightText}>
+                        <Date dateString={postData.date} />
+                    </div>
+                    <div dangerouslySetInnerHTML={{ __html: postData.contentHTML }} />
+                </article>
             </Layout>
     )
 }
